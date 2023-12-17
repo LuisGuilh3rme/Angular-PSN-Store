360a8fba58ad60c2099833aa414514366c27d52f
LuisGuilh3rme <luislebert01@outlook.com>
2023-12-17 18:23:13 -0300

    refact: organizing assets

--- src/data/menu-data.ts
+++ src/data/menu-data.ts
@@ -5,35 +5,35 @@ export const MenuData: MenuDataInterface[] = [
   {
     id: 1,
     title: "Ofertas",
-    iconUrl: "assets/sales_icon.svg",
+    iconUrl: "assets/icons/menu-dropdown/sales_icon.svg",
     menuType: menuType.games,
     url: "not-found"
   },
   {
     id: 2,
     title: "Catalogo de jogos",
-    iconUrl: "assets/game_catalog_icon.svg",
+    iconUrl: "assets/icons/menu-dropdown/game_catalog_icon.svg",
     menuType: menuType.games,
     url: "not-found"
   },
   {
     id: 3,
     title: "PS Plus",
-    iconUrl: "assets/ps_plus_icon.svg",
+    iconUrl: "assets/icons/menu-dropdown/ps_plus_icon.svg",
     menuType: menuType.games,
     url: "not-found"
   },
   {
     id: 4,
     title: "GitHub",
-    iconUrl: "assets/github_icon.svg",
+    iconUrl: "assets/icons/menu-dropdown/github_icon.svg",
     menuType: menuType.support,
     url: "https://github.com/luisguilh3rme"
   },
   {
     id: 5,
     title: "LinkedIn",
-    iconUrl: "assets/linkedin_icon.svg",
+    iconUrl: "assets/icons/menu-dropdown/linkedin_icon.svg",
     menuType: menuType.support,
     url: "https://www.linkedin.com/in/luis-guilherme-francisco-da-silva-70432522b"
   }
