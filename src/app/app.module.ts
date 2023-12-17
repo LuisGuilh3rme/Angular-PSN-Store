883e30966dc497d705b6b6ae12606d79f459218f
LuisGuilh3rme <luislebert01@outlook.com>
2023-12-17 00:30:26 -0300

    refact: organizing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
-import { TopBarComponent } from './shared/menu-bar/top-bar/top-bar.component';
-import { SideBarComponent } from './shared/menu-bar/side-bar/side-bar.component';
-import { MainProductComponent } from './components/main-product/main-product.component';
-import { GameCardComponent } from './shared/game-card/game-card.component';
-import { BrowseComponent } from './pages/browse/browse.component';
-import { GameCardModalComponent } from './shared/game-card/game-card-modal/game-card-modal.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { TopBarDropdownComponent } from './shared/menu-bar/top-bar/top-bar-dropdown/top-bar-dropdown.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { MenuBarComponent } from './components/shared/menu-bar/menu-bar.component';
+import { TopBarComponent } from './components/shared/menu-bar/top-bar/top-bar.component';
+import { SideBarComponent } from './components/shared/menu-bar/side-bar/side-bar.component';
+import { MainProductComponent } from './components/pages/home/main-product/main-product.component';
+import { GameCardComponent } from './components/shared/game-card/game-card.component';
+import { BrowseComponent } from './components//pages/browse/browse.component';
+import { GameCardModalComponent } from './components/shared/game-card/game-card-modal/game-card-modal.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+import { TopBarDropdownComponent } from './components/shared/menu-bar/top-bar/top-bar-dropdown/top-bar-dropdown.component';
 @NgModule({
   declarations: [
     AppComponent,
