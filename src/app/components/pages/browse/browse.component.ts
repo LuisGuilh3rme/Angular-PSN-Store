14e8f5f95e418a40dd870abd044d009750eefe22
LuisGuilh3rme <luislebert01@outlook.com>
2023-12-24 20:46:40 -0300

    feat: implements responsive layout

--- src/app/components/pages/browse/browse.component.ts
+++ src/app/components/pages/browse/browse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { GameData } from 'src/data/game-data';
 import { GameDataInterface } from 'src/interfaces/game-data-interface';
 
@@ -9,12 +9,15 @@ import { GameDataInterface } from 'src/interfaces/game-data-interface';
 })
 export class BrowseComponent implements OnInit, OnDestroy {
   public games: GameDataInterface[];
+  public gameCardDimension: number = 200;
 
   constructor() {
     this.games = GameData;
   }
 
+  @HostListener('window:resize')
   ngOnInit(): void {
+    this.gameCardDimension = window.innerWidth > 500 ? 200 : 100;
   }
 
   ngOnDestroy(): void {
