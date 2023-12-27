a6ed0e66680910badc2fb82cff9d53123d19bb34
LuisGuilh3rme <luislebert01@outlook.com>
2023-12-26 23:05:05 -0300

    feat: changes in top bar dropdown component redirection method

--- src/app/components/shared/menu-bar/top-bar/top-bar-dropdown/top-bar-dropdown.component.ts
+++ src/app/components/shared/menu-bar/top-bar/top-bar-dropdown/top-bar-dropdown.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuData } from 'src/data/menu-data';
 import { menuType } from 'src/enums/menu-type-enum';
 import { MenuDataInterface } from 'src/interfaces/menu-data-interface';
@@ -11,9 +12,17 @@ import { MenuDataInterface } from 'src/interfaces/menu-data-interface';
 export class TopBarDropdownComponent implements OnInit {
   @Input() public menuItemType?: menuType;
   public menuItems?: MenuDataInterface[];
-  constructor() { }
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.menuItems = MenuData.filter(item => item.menuType == this.menuItemType);
+  }
+
+  linkRedirect(url: string) {
+    if (url.includes('https'))
+      window.location.href = url;
+    else
+      this.router.navigate(['/', url]);
   }
 }
